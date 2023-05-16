cc52a440c2c500be7dc4b7c17af14d55054c1934
Koray <74928882+koraydincc@users.noreply.github.com>
2023-05-17 00:54:48 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,9 +4,10 @@ const newData = []
 
 let readListBtn = document.getElementById('listeleBtn');
 let info = document.getElementById('registerInfo')
-let kayitList = document.getElementById('kayit-List')
+let kayitList = document.getElementById('kayit-list')
 let successful = document.getElementById('registerSuccessful')
 let danger = document.getElementById('registerDanger')
+let elemanSayisi = document.getElementById('toplamEleman')
 
 document.getElementById('kaydetBtn').addEventListener('click', function(){
     let pasword = document.getElementById('passwordInput')
@@ -30,7 +31,7 @@ document.getElementById('kaydetBtn').addEventListener('click', function(){
 
     if (!testEmailValidity(email.value)) {
         info.innerHTML = 'Lütfen geçerli bir email adresi giriniz !';
-        return false;
+         return false;
     }
 
    
@@ -41,20 +42,49 @@ document.getElementById('kaydetBtn').addEventListener('click', function(){
     email.value = '';
     pasword.value = '';
 
-    setTimeout(() => {  
+    setTimeout(() => {  //setTimeout bir kez çalıştırır
         successful.innerHTML = '';
     }, 1000);
 
     console.log(newData)
 
     function testEmailValidity(email) {
-        const re = /\S+@\S+\.\S+/;
+         const re = /\S+@\S+\.\S+/;
         return re.test(String(email).toLowerCase());
-        //  '\S+' Bir veya daha fazla karakteri eşler, boşluk olmayan karakter anlamına gelir
+         //  '\S+' Bir veya daha fazla karakteri eşler, boşluk olmayan karakter anlamına gelir
         //   \. nokta karakterini eşleştirir 
-    }
+     }
 
 })
+
+document.getElementById('listeleBtn').addEventListener('click',function(){
+
+    let temp = ''; //Kayıtları yazdırmak için boş string oluşturduk
+    let counter = 0; //Sırayla  yazdırması için sayaç oluşturduk
+    
+
+    
+   
+    let listeokuyucu = setInterval(function()  { //setInterval belirli aralıklarla çalıştırır
+
+        temp+= `<li class="list-group-item" id="kayit-list">Email adresi : ${newData[counter]}</li> `
+        counter++;
+        temp+= `<li class="list-group-item" id="kayit-list">Password : ${newData[counter]}</li> `
+        counter++;
+    
+        kayitList.innerHTML = temp
+
+    if(counter == newData.length){
+       
+        elemanSayisi.innerHTML = `Toplam Üye = ${newData.length/2}`
+        clearInterval(listeokuyucu)
+    }   
+        
+    }, 1000);
+
+})
+
+
 
 
 
